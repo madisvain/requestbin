9a06788073a4bff780ed54b374104108f2730a33
Madis Väin <madisvain@gmail.com>
2018-09-19 21:53:21 +0300

    add filtering by bin test for requests

--- requestbin/tests/test_requests_api.py
+++ requestbin/tests/test_requests_api.py
@@ -10,6 +10,37 @@ class TestRequestsApi(TestBase):
         request, response = app.test_client.get("/api/requests/")
         assert response.status == 200
         assert isinstance(response.json, list)
+    
+    def test_list_filter(self):
+        b1 = Bin.create(name="random1", private=True)
+        b2 = Bin.create(name="random1", private=True)
+        r1 = Request.create(
+            bin=b1,
+            method="get",
+            ip="127.0.0.1",
+            json={},
+            headers={},
+            body=b"",
+            port="8000",
+            size=100,
+            time=100
+        )
+        r1 = Request.create(
+            bin=b2,
+            method="get",
+            ip="127.0.0.1",
+            json={},
+            headers={},
+            body=b"",
+            port="8000",
+            size=100,
+            time=100
+        )
+
+        request, response = app.test_client.get("/api/requests/", params={"bin": str(b1.id)})
+        assert response.status == 200
+        assert isinstance(response.json, list)
+        assert len(response.json) == 1
 
     def test_detail(self):
         b = Bin.create(name="random", private=True)
